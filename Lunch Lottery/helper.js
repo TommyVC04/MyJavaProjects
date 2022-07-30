1718446278066f481783d0f9396baf19ec09cf36
TommyVC04 <thomas.j.vancamp@gmail.com>
2022-07-29 23:40:37 -0500

    Small Stuff

--- Lunch Lottery/helper.js
+++ Lunch Lottery/helper.js
@@ -1,3 +1,4 @@
 function printStudent() {
     document.getElementById("student").innerHTML = document.getElementById("fname") + " " + document.getElementById("lname") + " " + document.getElementById("stuid");
+    return;
 }
