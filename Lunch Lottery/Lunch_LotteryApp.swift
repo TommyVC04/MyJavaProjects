cec435f59a8518c5226ab165ed8442c64c9b3166
TommyVC04 <thomas.j.vancamp@gmail.com>
2022-08-29 09:44:25 -0500

    Initial Commit

---
+++ Lunch Lottery/Lunch_LotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lunch_LotteryApp.swift
+//  Lunch Lottery
+//
+//  Created by 2390004 on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Lunch_LotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lunch LotteryUITests/Lunch_LotteryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lunch_LotteryUITestsLaunchTests.swift
+//  Lunch LotteryUITests
+//
+//  Created by 2390004 on 8/29/22.
+//
+
+import XCTest
+
+class Lunch_LotteryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
