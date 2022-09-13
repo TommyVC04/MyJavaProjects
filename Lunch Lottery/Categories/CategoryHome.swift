189bedf9b789a8fa8834527133dad1f1ef5e4d1d
TommyVC04 <thomas.j.vancamp@gmail.com>
2022-09-13 09:48:56 -0500

    Complex UI Section 2

---
+++ Lunch Lottery/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Lunch Lottery
+//
+//  Created by TommyVC04 on 9/13/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView  {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
