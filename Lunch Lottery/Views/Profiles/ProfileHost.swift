2fb22ac7ac70b1b1adc8568c8446060fee8ea011
TommyVC04 <thomas.j.vancamp@gmail.com>
2022-09-14 10:21:11 -0500

    Complex UI Section 4

---
+++ Lunch Lottery/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileHost.swift
+//  Lunch Lottery
+//
+//  Created by TommyVC04 on 9/14/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+            .environmentObject(ModelData())
+    }
+}
