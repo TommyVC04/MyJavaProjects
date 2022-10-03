4dc5d15d736297426f7846e0d6bb1ac1fa8de095
TommyVC04 <thomas.j.vancamp@gmail.com>
2022-10-03 09:51:41 -0500

    Fix Model Data Error, Buttons

--- Lunch Lottery/Views/Profiles/PageView.swift
+++ Lunch Lottery/Views/Profiles/PageView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
-    @State private var currentPage = 1
+    @State private var currentPage = 0
 
     var body: some View {
-        ZStack (alignment: .bottomTrailing){
+        ZStack(alignment: .bottomTrailing) {
             PageViewController(pages: pages, currentPage: $currentPage)
             PageControl(numberOfPages: pages.count, currentPage: $currentPage)
                 .frame(width: CGFloat(pages.count * 18))
